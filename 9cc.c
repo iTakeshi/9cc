0ee3c9ce9e491867d844b970b42f30309c88585c
iTakeshi <itakeshi.net@gmail.com>
2019-07-11 00:53:59 +0900

    step 03: tokenize

--- 9cc.c
+++ 9cc.c
@@ -1,34 +1,119 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+    TK_RESERVED, // symbols
+    TK_NUM,      // integers
+    TK_EOF,      // EOF
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+    TokenKind kind;
+    Token * next;
+    int val;
+    char * str;
+};
+
+Token * token;
+
+void error(char * fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+bool consume(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op) {
+        return false;
+    }
+    token = token->next;
+    return true;
+}
+
+void expect(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op) {
+        error("Unexpected character; expected '%c'", op);
+    }
+    token = token->next;
+}
+
+int expect_number() {
+    if (token->kind != TK_NUM) {
+        error("Unexpected toekn; expected a number");
+    }
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+bool at_eof() {
+    return token->kind == TK_EOF;
+}
+
+Token * new_token(TokenKind kind, Token * cur, char * str) {
+    Token * tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->str = str;
+    cur->next = tok;
+    return tok;
+}
+
+Token * tokenize(char * p) {
+    Token head;
+    head.next = NULL;
+    Token * cur = &head;
+
+    while (*p) {
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        if (*p == '+' || *p == '-') {
+            cur = new_token(TK_RESERVED, cur, p++);
+            continue;
+        }
+
+        if (isdigit(*p)) {
+            cur = new_token(TK_NUM, cur, p);
+            cur->val = strtol(p, &p, 10);
+            continue;
+        }
+
+        error("Cannot tokenize");
+    }
+
+    new_token(TK_EOF, cur, p);
+    return head.next;
+}
 
 int main(int argc, char ** argv) {
     if (argc != 2) {
-        fprintf(stderr, "Wrong number of arguments.");
-        return 1;
+        error("Wrong number of arguments.");
     }
 
-    char * p = argv[1];
+    token = tokenize(argv[1]);
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, %ld\n", strtol(p, &p, 10));
+    printf("  mov rax, %ld\n", expect_number());
 
-    while (*p) {
-        if (*p == '+') {
-            p++;
-            printf("  add rax, %ld\n", strtol(p, &p, 10));
-            continue;
+    while (!at_eof()) {
+        if (consume('+')) {
+            printf("  add rax, %ld\n", expect_number());
+        } else {
+            expect('-');
+            printf("  sub rax, %ld\n", expect_number());
         }
-
-        if (*p == '-') {
-            p++;
-            printf("  sub rax, %ld\n", strtol(p, &p, 10));
-            continue;
-        }
-
-        fprintf(stderr, "Unexpected character.");
-        return 1;
     }
 
     printf("  ret\n");
