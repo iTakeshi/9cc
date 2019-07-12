5ddaea5f319ef5446c23b76b96bfaa3adde14cf0
iTakeshi <itakeshi.net@gmail.com>
2019-07-12 22:10:07 +0900

    step 05-1: stack machine

--- 9cc.c
+++ 9cc.c
@@ -68,10 +68,6 @@ int expect_number() {
     return val;
 }
 
-bool at_eof() {
-    return token->kind == TK_EOF;
-}
-
 Token * new_token(TokenKind kind, Token * cur, char * str) {
     Token * tok = calloc(1, sizeof(Token));
     tok->kind = kind;
@@ -109,6 +105,74 @@ Token * tokenize(char * p) {
     return head.next;
 }
 
+typedef enum {
+    ND_ADD,
+    ND_SUB,
+    ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+    NodeKind kind;
+    Node * lhs;
+    Node * rhs;
+    int val;
+};
+
+Node * new_node(NodeKind kind, Node * lhs, Node * rhs) {
+    Node * node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node * new_node_num(int val) {
+    Node * node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node * expr() {
+    Node * node = new_node_num(expect_number());
+
+    while (true) {
+        if (consume('+')) {
+            node = new_node(ND_ADD, node, expr());
+        } else if (consume('-')) {
+            node = new_node(ND_SUB, node, expr());
+        } else {
+            return node;
+        }
+    }
+}
+
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("  push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch (node->kind) {
+        case ND_ADD:
+            printf("  add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("  sub rax, rdi\n");
+            break;
+    }
+
+    printf("  push rax\n");
+}
+
 int main(int argc, char ** argv) {
     if (argc != 2) {
         error("Wrong number of arguments.");
@@ -116,21 +180,15 @@ int main(int argc, char ** argv) {
 
     user_input = argv[1];
     token = tokenize(argv[1]);
+    Node * node = expr();
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, %ld\n", expect_number());
 
-    while (!at_eof()) {
-        if (consume('+')) {
-            printf("  add rax, %ld\n", expect_number());
-        } else {
-            expect('-');
-            printf("  sub rax, %ld\n", expect_number());
-        }
-    }
+    gen(node);
 
+    printf("  pop rax\n");
     printf("  ret\n");
     return 0;
 }
